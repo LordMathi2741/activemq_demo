9e9abc9da1547335948ed3c782717bc3e0dc49ef
Mathias Jave Diaz <u202212721@upc.edu.pe>
2026-03-28 07:08:42 -0500

    feat: exposed web console port

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f218b662f6b27cbcfffb924a5c2b71a48bf8788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0615b15cd342cf4b2a1f2c75ee2e3655b8ff97f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Receiver/obj/Debug/net8.0/Receiver.AssemblyInfo.cs
+++ Receiver/obj/Debug/net8.0/Receiver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Receiver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f218b662f6b27cbcfffb924a5c2b71a48bf8788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0615b15cd342cf4b2a1f2c75ee2e3655b8ff97f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Receiver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Receiver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
